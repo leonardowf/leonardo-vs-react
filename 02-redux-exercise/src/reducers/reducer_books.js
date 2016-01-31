ba6c56f316eab32f223f7de5d1c56bf5608e6208
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2016-01-31 14:40:12 -0200

    Adds redux exercise

---
+++ 02-redux-exercise/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'The Dark Tower' },
+    { title: 'Harry Potter' },
+    { title: 'Javascript the Good Parts' },
+    { title: '1984' },
+    { title: 'Animal Farm'}
+  ]
+}
