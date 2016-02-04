08fc02f1b765ea634a92862d16b703be26478e29
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2016-02-03 23:24:03 -0200

    Configures react router to work with browserHistory

--- 04-blog-exercise/src/index.js
+++ 04-blog-exercise/src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
 
-import App from './components/app';
+import routes from './routes';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={ browserHistory } routes={ routes } />
   </Provider>
   , document.querySelector('.container'));
--- 04-blog-exercise/src/routes.js
+++ 04-blog-exercise/src/routes.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
 
-export default {
-  <Route path="/" component={App}/>
+const login = () => {
+  console.log("entrei em login");
 }
+
+export default (
+  <Route path="/" component={ App }>
+    <IndexRoute component={ PostsIndex } />
+    <Route path="posts" component={ PostsIndex } onEnter={ login } />
+  </Route>
+);
