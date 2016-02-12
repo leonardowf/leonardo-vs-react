df8496edea5c0c60b3bbae1ab6798196f64b440b
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2016-02-11 23:25:09 -0200

    Adds boilerplate project containing bourbon and neat

---
+++ 06-react-redux-starter-kit/src/views/LoginView/LoginView.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class LoginView extends Component {
+  render () {
+    return (
+      <section>
+        <aside>What is it about?</aside>
+        <article>Neat is an open source semantic grid framework built on top of Sass and Bourbon…</article>
+      </section>
+    )
+  }
+}
