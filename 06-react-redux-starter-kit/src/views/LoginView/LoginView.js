3eaf804efb23978c2953acdcced43aec33aa09a0
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2016-02-13 01:47:47 -0200

    Splits login tabs into two different components

--- 06-react-redux-starter-kit/src/views/LoginView/LoginView.js
+++ 06-react-redux-starter-kit/src/views/LoginView/LoginView.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { reduxForm } from 'redux-form'
+
+import LoginTabForm from './LoginTabForm'
+import SignupTabForm from './SignupTabForm'
 
 const LOGIN_TAB = 'LOGIN_TAB'
 const SIGNUP_TAB = 'SIGNUP_TAB'
@@ -31,60 +33,30 @@ class LoginView extends Component {
     return 'tab-content'
   }
 
-  onSubmitLogin(loginProps) {
+  onSubmitLogin (loginProps) {
     console.log(loginProps)
   }
 
-  render () {
-    const {fields: {email, password}, handleSubmit} = this.props
+  onSelectTab (tabType) {
+    this.setState({activeTab: tabType})
+  }
 
+  render () {
     return (
       <div className='box'>
         <ul className='accordion-tabs-minimal'>
-          <li className='tab-header-and-content'>
-            <a href='#' className={this.tabClass(LOGIN_TAB)} onClick={() => this.setState({activeTab: LOGIN_TAB})}>
-              Login
-            </a>
-            <div className={this.tabContentClass(LOGIN_TAB)}>
-              <form onSubmit={handleSubmit(this.onSubmitLogin.bind(this))}>
-                <input type='text' placeholder='E-mail' {...email}>
-                </input>
-
-                <input type='password' placeholder='Senha' {...password}>
-                </input>
-
-                <button type='submit'>Entrar</button>
-              </form>
-            </div>
-          </li>
-          <li className='tab-header-and-content'>
-            <a href='#' className={this.tabClass(SIGNUP_TAB)} onClick={() => this.setState({activeTab: SIGNUP_TAB})}>
-              Registre-se
-            </a>
-            <div className={this.tabContentClass(SIGNUP_TAB)}>
-              <form>
-                <input type='text' placeholder='E-mail'>
-                </input>
-
-                <input type='text' placeholder='Nome da pizzaria'>
-                </input>
-
-                <input type='password' placeholder='Senha'>
-                </input>
-
-                <button type='submit'>Registrar</button>
-              </form>
-            </div>
-          </li>
+          <LoginTabForm
+            isSelected={this.isActive(LOGIN_TAB)}
+            onClick={() => { this.onSelectTab(LOGIN_TAB) }}
+          />
+          <SignupTabForm
+            isSelected={this.isActive(SIGNUP_TAB)}
+            onClick={() => { this.onSelectTab(SIGNUP_TAB) }}
+          />
         </ul>
       </div>
     )
   }
 }
-
-LoginView = reduxForm({ // <----- THIS IS THE IMPORTANT PART!
-  form: 'LoginViewForm',                           // a unique name for this form
-  fields: ['email', 'password'] // all the fields in your form
-})(LoginView)
 
 export default LoginView
