513109a007b4f440ca48629ae96623ac71570d16
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2016-02-12 22:07:03 -0200

    Adds login/signup component

--- 06-react-redux-starter-kit/src/views/LoginView/LoginView.js
+++ 06-react-redux-starter-kit/src/views/LoginView/LoginView.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react'
 
+const LOGIN_TAB = 'LOGIN_TAB'
+const SIGNUP_TAB = 'SIGNUP_TAB'
+
 export default class LoginView extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {activeTab: LOGIN_TAB}
+  }
+
+  isActive (tabType) {
+    return tabType === this.state.activeTab
+  }
+
+  tabClass (tabType) {
+    if (this.isActive(tabType)) {
+      return 'tab-link is-active'
+    }
+
+    return 'tab-link'
+  }
+
+  tabContentClass (tabType) {
+    if (this.isActive(tabType)) {
+      return 'tab-content is-open'
+    }
+
+    return 'tab-content'
+  }
+
   render () {
     return (
       <div className='box'>
         <ul className='accordion-tabs-minimal'>
           <li className='tab-header-and-content'>
-            <a href='#' className='tab-link is-active'>Login</a>
-            <div className='tab-content is-open'>
+            <a href='#' className={this.tabClass(LOGIN_TAB)} onClick={() => this.setState({activeTab: LOGIN_TAB})}>
+              Login
+            </a>
+            <div className={this.tabContentClass(LOGIN_TAB)}>
               <form>
-                <input type='text'>
+                <input type='text' placeholder='E-mail'>
                 </input>
 
-                <input type='password'>
+                <input type='password' placeholder='Senha'>
                 </input>
 
                 <button type='submit'>Entrar</button>
@@ -20,23 +51,22 @@ export default class LoginView extends Component {
             </div>
           </li>
           <li className='tab-header-and-content'>
-            <a href='#' className='tab-link'>Registre-se</a>
-            <div className='tab-content'>
-              <p>
-                Ut laoreet augue et neque pretium non sagittis nibh pulvinar.
-                Etiam ornare tincidunt orci quis ultrices. Pellentesque ac
-                sapien ac purus gravida ullamcorper. Duis rhoncus sodales
-                lacus, vitae adipiscing tellus pharetra sed. Praesent bibendum
-                lacus quis metus condimentum ac accumsan orci vulputate.
-                Aenean fringilla massa vitae metus facilisis congue. Morbi
-                placerat eros ac sapien semper pulvinar. Vestibulum facilisis,
-                ligula a molestie venenatis, metus justo ullamcorper ipsum,
-                congue aliquet dolor tortor eu neque. Sed imperdiet, nibh ut
-                vestibulum tempor, nibh dui volutpat lacus, vel gravida magna
-                justo sit amet quam. Quisque tincidunt ligula at nisl imperdiet
-                sagittis. Morbi rutrum tempor arcu, non ultrices sem semper a.
-                Aliquam quis sem mi.
-              </p>
+            <a href='#' className={this.tabClass(SIGNUP_TAB)} onClick={() => this.setState({activeTab: SIGNUP_TAB})}>
+              Registre-se
+            </a>
+            <div className={this.tabContentClass(SIGNUP_TAB)}>
+              <form>
+                <input type='text' placeholder='E-mail'>
+                </input>
+
+                <input type='text' placeholder='Nome da pizzaria'>
+                </input>
+
+                <input type='password' placeholder='Senha'>
+                </input>
+
+                <button type='submit'>Registrar</button>
+              </form>
             </div>
           </li>
         </ul>
